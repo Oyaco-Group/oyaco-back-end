6b4c99a96b0e1b544c8a0cbc4a337da8bc81cd2d
johnathanjohnathan <johnathanvingttrois@gmail.com>
2024-07-12 22:39:21 +0700

    continuing order feature

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -3,17 +3,10 @@ class OrderController {
   // admin create order
   static async createOrder(req, res, next) {
     try {
-      const {
-        user_id,
-        complaint_id,
-        payment_type,
-        order_status,
-        buyer_status,
-      } = req.body;
+      const { user_id, payment_type, order_status, buyer_status } = req.body;
 
       const order = await OrderService.createOrder({
         user_id,
-        complaint_id,
         payment_type,
         order_status,
         buyer_status,
@@ -34,7 +27,7 @@ class OrderController {
       const orders = await OrderService.getOrder(page);
 
       res.status(200).json({
-        massage: "success get all orders",
+        message: "success get all orders",
         data: orders,
       });
     } catch (err) {
@@ -44,20 +37,73 @@ class OrderController {
   // admin get order by id
   static async getOneOrder(req, res, next) {
     try {
-      const {id} = req.params;
+      const { id } = req.params;
 
       const order = await OrderService.getOneOrder(id);
 
       res.status(200).json({
-        massage: "success get order",
+        message: "success get order",
         data: order,
       });
-    } catch (err) {}
+    } catch (err) {
+      next(err);
+    }
   }
   // admin get nomor resi
   // user get email and no resi
-  // user update order status
+  // user update status order
+  static async updateOrderStatus(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { order_status } = req.body;
+
+      const order = await OrderService.updateOrderStatus({
+        id,
+        order_status,
+      });
+
+      res.status(200).json({
+        message: "success update order status",
+        data: order,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+  // admin update order
+  static async updateOrder(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { user_id, payment_type, order_status, buyer_status } = req.body;
+
+      const order = await OrderService.updateOrder({
+        id,
+        user_id,
+        payment_type,
+        order_status,
+        buyer_status,
+      });
+
+      res.status(200).json({
+        message: "success update order",
+        data: order,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
   // delete order
+  static async deleteOrder(req, res, next) {
+    try {
+      const { id } = req.params;
+      await OrderService.deleteOrder(id);
+      res.status(200).json({
+        message: "Successfully delete order",
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = OrderController;
