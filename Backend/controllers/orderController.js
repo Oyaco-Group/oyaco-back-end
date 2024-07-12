72e40992cdb1bcaa8435b64ed8b0243ad1147786
Johnathan <johnathanvingttrois@gmail.com>
2024-07-12 22:58:12 +0700

    Test conflict part 1 (#14)
    * adding order feature
    
    * adding get order
    
    * updating relation between order and complaint
    
    * adding get order by id
    
    * updating inventory table
    
    * continuing order feature
    
    * Adding emailAndResiController.js
    
    * adding function in emailAndResiController.js
    
    * test conflict part 1

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const OrderService = require("../services/orderService");
 class OrderController {
+
   // admin create order
   static async createOrder(req, res, next) {
     try {
@@ -20,6 +21,7 @@ class OrderController {
       next(err);
     }
   }
+
   // admin get order
   static async getOrder(req, res, next) {
     try {
@@ -34,6 +36,7 @@ class OrderController {
       next(err);
     }
   }
+
   // admin get order by id
   static async getOneOrder(req, res, next) {
     try {
@@ -49,8 +52,7 @@ class OrderController {
       next(err);
     }
   }
-  // admin get nomor resi
-  // user get email and no resi
+  
   // user update status order
   static async updateOrderStatus(req, res, next) {
     try {
@@ -70,6 +72,7 @@ class OrderController {
       next(err);
     }
   }
+
   // admin update order
   static async updateOrder(req, res, next) {
     try {
@@ -98,6 +101,7 @@ class OrderController {
     try {
       const { id } = req.params;
       await OrderService.deleteOrder(id);
+      console.log(id, '<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
       res.status(200).json({
         message: "Successfully delete order",
       });
