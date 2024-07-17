181f11af3bf8caf730c042654d85bb0180b1112d
adolfalmeida <143357238+adolfalmeida@users.noreply.github.com>
2024-07-17 14:50:47 +0700

    Feature/transaction (#32)
    * inventory crud
    
    * transaction
    
    * update isi transService
    
    * update isi transService
    
    * fixed some code in transaction feature
    
    * fixed some errors in code transService
    
    * Added error handlers, logic so that product cannot be less than 0, and logic so that when a prodcut is 0 isdelete is changed to true in inventory
    
    * Added error handlers, logic so that product cannot be less than 0, and logic so that when a prodcut is 0 isdelete is changed to true in inventory
    
    * Updated some code and added error handlers
    
    * Menambahkan comment dan template input postman agar lebih mudah dipakai
    
    * Menambahkan comment dan template input postman agar lebih mudah dipakai
    
    * Fixed some typo
    
    * Transactions features
    
    * Added get and sort for transaction

--- Backend/routes/transactionRoute.js
+++ Backend/routes/transactionRoute.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const TransController = require("../controllers/transactionController");
 
+router.get("/", TransController.getAllTransactions);
+router.get("/:id", TransController.getTransactionById);
+router.get("/sort/highest", TransController.sortHighest);
+router.get("/sort/lowest", TransController.sortLowest);
 router.post("/", TransController.createTrans);
 
 module.exports = router;
