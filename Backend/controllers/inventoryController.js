9f998dfae6edb76c570d1f1a938a8bb19ad61026
adolfalmeida <143357238+adolfalmeida@users.noreply.github.com>
2024-07-26 17:01:50 +0700

    Feature/transaction (#53)
    * inventory crud
    
    * transaction
    
    * update isi transService
    
    * update isi transService
    
    * fixed some code in transaction feature
    
    * fixed some errors in code transService
    
    * Added error handlers, logic so that product cannot be less than 0, and logic so that when a prodcut is 0 isdelete is changed to true in inventory
    
    * Added error handlers, logic so that product cannot be less than 0, and logic so that when a prodcut is 0 isdelete is changed to true in inventory
    
    * Updated some code and added error handlers
    
    * Menambahkan comment dan template input postman agar lebih mudah dipakai
    
    * Menambahkan comment dan template input postman agar lebih mudah dipakai
    
    * Fixed some typo
    
    * Transactions features
    
    * Added get and sort for transaction
    
    * Added getByWarehouseId
    
    * added some features to transaction
    
    * added updateExpirationStatus to transactionService
    
    * fixed some errors
    
    * resolved conflicts
    
    * fixed some errors
    
    * added some features to feature/transaction
    
    * added some improvements
    
    * added some features

--- Backend/controllers/inventoryController.js
+++ Backend/controllers/inventoryController.js
@@ -3,12 +3,7 @@ const InventoryService = require("../services/inventoryService");
 class InventoryController {
   static async createStock(req, res, next) {
     try {
-      const {
-        master_product_id,
-        warehouse_id,
-        quantity,
-        isdelete,
-      } = req.body;
+      const { master_product_id, warehouse_id, quantity, isdelete } = req.body;
 
       const inventory = await InventoryService.createStock({
         master_product_id,
@@ -31,7 +26,7 @@ class InventoryController {
       const inventory = await InventoryService.getAllStock(page);
 
       res.status(200).json({
-        massage: "success get all stock",
+        message: "success get all stock",
         data: inventory,
       });
     } catch (err) {
@@ -80,9 +75,10 @@ class InventoryController {
 
   static async getStockByWarehouse(req, res, next) {
     try {
-      const { id } = req.params;
+      const page = req.query.page
+      const { warehouse_id } = req.params;
 
-      const inventory = await InventoryService.getStockByWarehouse(id);
+      const inventory = await InventoryService.getStockByWarehouse(warehouse_id, page);
 
       res.status(200).json({
         message: "success get stock",
@@ -96,12 +92,7 @@ class InventoryController {
   static async editStock(req, res, next) {
     try {
       const { id } = req.params;
-      const {
-        master_product_id,
-        warehouse_id,
-        quantity,
-        isdelete,
-      } = req.body;
+      const { master_product_id, warehouse_id, quantity, isdelete } = req.body;
 
       const editStock = await InventoryService.editStock({
         id,
@@ -135,10 +126,6 @@ class InventoryController {
       next(err);
     }
   }
-
-
-  
-
 }
 
 module.exports = InventoryController;
