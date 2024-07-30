e5162b637cacf1bc8d24e55ad302868ff165e472
adolfalmeida <143357238+adolfalmeida@users.noreply.github.com>
2024-07-30 22:03:02 +0700

    Feature/transaction (#62)
    * inventory crud
    
    * transaction
    
    * update isi transService
    
    * update isi transService
    
    * fixed some code in transaction feature
    
    * fixed some errors in code transService
    
    * Added error handlers, logic so that product cannot be less than 0, and logic so that when a prodcut is 0 isdelete is changed to true in inventory
    
    * Added error handlers, logic so that product cannot be less than 0, and logic so that when a prodcut is 0 isdelete is changed to true in inventory
    
    * Updated some code and added error handlers
    
    * Menambahkan comment dan template input postman agar lebih mudah dipakai
    
    * Menambahkan comment dan template input postman agar lebih mudah dipakai
    
    * Fixed some typo
    
    * Transactions features
    
    * Added get and sort for transaction
    
    * Added getByWarehouseId
    
    * added some features to transaction
    
    * added updateExpirationStatus to transactionService
    
    * fixed some errors
    
    * resolved conflicts
    
    * fixed some errors
    
    * added some features to feature/transaction
    
    * added some improvements
    
    * added some features
    
    * fix: fixed error messages in transactionService

--- Backend/controllers/inventoryController.js
+++ Backend/controllers/inventoryController.js
@@ -75,6 +75,7 @@ class InventoryController {
 
   static async getStockByWarehouse(req, res, next) {
     try {
+<<<<<<< HEAD
       const page = req.query.page;
       const { warehouse_id } = req.params;
 
@@ -96,6 +97,13 @@ class InventoryController {
     try {
       const { id } = req.params;
       const inventory = await InventoryService.getStockByProductId(id);
+=======
+      const page = req.query.page
+      const { warehouse_id } = req.params;
+
+      const inventory = await InventoryService.getStockByWarehouse(warehouse_id, page);
+
+>>>>>>> 2ec4f2edc140574d5a05b6a89b0372211a947327
       res.status(200).json({
         message: "success get stock",
         data: inventory,
